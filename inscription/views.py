4d092fd511b70c9304b50806656c633dd251f872
manuelenrq9 <manuel.enrqt@gmail.com>
2023-11-26 10:11:53 -0400

    changes

--- inscription/views.py
+++ inscription/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import estudiante
 
 # Create your views here.
+def insert_student(request):
+    if request.method == 'POST':
+        nombre = request.POST['NOMBRE']
+        cedula = request.POST['CEDULA']
+        nacimiento = request.POST['dob']
+        direccion = request.POST['DIRECCION']
+        correo = request.POST['CORREO']
+        # Retrieve other form fields similarly
+
+        # Create a new estudiante object and save it to the database
+        new_student = estudiante(nombre_apellido=nombre, cedula=cedula, nacimiento=nacimiento, direccion=direccion, correo=correo)
+        new_student.save()
+        return HttpResponse('Student inserted successfully')
+    else:
+        return render(request, 'your_template.html')
